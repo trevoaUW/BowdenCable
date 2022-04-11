04552d5397770dadd38585519292d1d67875ebbc
trevoa99 <trevoa@uw.edu>
2022-04-10 23:33:23 -0700

    Fixed root locus and sys tf

--- controller_design.m
+++ controller_design.m
@@ -19,30 +19,34 @@ param.J = 0.00034;
 plant = tf([1], [param.J 0 0]);
 omega_c = 15; % rad/s;
 opt = pidtuneOptions('DesignFocus', 'reference-tracking');
-c = pidtune(plant, 'pidf', omega_c, opt);
+pid = pidtune(plant, 'pidf', omega_c, opt)
 
-param.Kp = c.Kp;
-param.Ki = c.Ki;
-param.Kd = c.Kd;
-param.N = 1/c.Tf;
+param.Kp = pid.Kp;
+param.Ki = pid.Ki;
+param.Kd = pid.Kd;
+param.N = 1/pid.Tf;
 
 %% Acceleration Controller Design
-ol_no_accel = c*plant;
+ol_no_accel = pid*plant;
 cl_no_accel = feedback(ol_no_accel, 1);
 omega_b = 5*bandwidth(cl_no_accel);
-param.Ka = 50;
+param.Ka = 10;
 param.tau = 1/omega_b;
 accel_cont = tf([param.Ka], [param.tau, 1]);
 
 %% Accel Root Locus
-ol_accel = c*accel_cont*plant;
+ol_accel = pid*accel_cont*plant;
 cl_accel = feedback(ol_accel, 1);
 
 dot = tf([1 0], [1]);
 double_dot = tf([1 0 0], [1]);
 
-C = feedback(plant, dot);
-D = feedback(accel_cont*param.J*C, double_dot);
+% C = feedback(plant, dot);
+D = feedback(accel_cont*param.J*plant, double_dot);
 
-rl_ol_tf = c*(1/param.J)*D;
+rl_ol_tf = pid*(1/param.J)*D;
 whole_sys_tf = feedback(rl_ol_tf, 1);
+
+%% Test
+step(whole_sys_tf)
+xlim([0, 1])
